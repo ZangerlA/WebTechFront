920e4e12f5d5dfda3a1ea66fb323e2707bfe30fa
Grossmann, Maximilian Josef <maxi.grossmann@hotmail.com>
2020-12-27 15:13:05 +0100

    Add CSS Reset and Register Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {RouterModule} from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
