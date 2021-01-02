fe964ce5b16613de22adab7afcd05d85b844b018
Grossmann, Maximilian Josef <maxi.grossmann@hotmail.com>
2021-01-02 12:47:32 +0100

    Update Nav Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,31 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { FlexLayoutModule} from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule} from '@angular/material/form-field';
-import { MatInputModule} from '@angular/material/input';
-import { MatButtonModule} from '@angular/material/button';
-import { MatCardModule} from '@angular/material/card';
-import { MatIconModule} from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import {ReactiveFormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AuthInterceptor } from './services/authconfig.interceptor.service';
 import { AuthGuard } from './services/auth.guard';
 import { HomeComponent } from './components/home/home.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { NavbarHeaderComponent } from './components/navbar-header/navbar-header.component';
-import { NavbarBodyComponent } from './components/navbar-body/navbar-body.component';
+import { NavbarComponent } from './components/navigation/navbar.component';
+import { NavbarHeaderComponent } from './components/navigation-toolbar/navbar-header.component';
+import { NavbarBodyComponent } from './components/navigation-sidebar/navbar-body.component';
+import { HomeWelcomeComponent } from './components/home-welcome/home-welcome.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+
+
 
 const routes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent }
+    { path: 'register', component: RegisterComponent },
+    { path: 'nav', component: NavbarHeaderComponent}
   ];
 
 
@@ -37,7 +44,8 @@ const routes: Routes = [
     HomeComponent,
     NavbarComponent,
     NavbarHeaderComponent,
-    NavbarBodyComponent
+    NavbarBodyComponent,
+    HomeWelcomeComponent
   ],
   imports: [
     HttpClientModule,
@@ -50,7 +58,10 @@ const routes: Routes = [
     MatButtonModule,
     MatCardModule,
     MatIconModule,
-    ReactiveFormsModule
+    MatSidenavModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatListModule
   ],
   providers: [
     {
