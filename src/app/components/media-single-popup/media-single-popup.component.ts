fe58be88f2a96ac57281101612ea39d582dda140
magrossmann <maxi.grossmann@hotmail.com>
2021-01-16 23:19:46 +0100

    Update MediaPopup and Add Review Model/Service

--- src/app/components/media-single-popup/media-single-popup.component.ts
+++ src/app/components/media-single-popup/media-single-popup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {Media} from "../../models/media.model";
 import {MAT_DIALOG_DATA} from "@angular/material/dialog";
+import {ReviewService} from "../../services/review.service";
+import {Review} from "../../models/review.model";
 
 @Component({
   selector: 'app-media-single-popup',
@@ -9,14 +11,20 @@ import {MAT_DIALOG_DATA} from "@angular/material/dialog";
 })
 export class MediaSinglePopupComponent implements OnInit {
   media: Media;
+  reviews: Review[];
+  ownReview: Review;
+  ReviewMessage: string = 'Review added!';
 
-  constructor(@Inject(MAT_DIALOG_DATA) public mediainc: Media) {
+  constructor(@Inject(MAT_DIALOG_DATA) public mediainc: Media, private reviewService: ReviewService) {
     this.media = mediainc;
-    console.log(this.media)
   }
 
   ngOnInit(): void {
+    this.reviewService.getReview(this.media.id).subscribe(res => this.reviews = res);
   }
 
-
+  setReview(){
+    this.ownReview.MediumId = this.media.id;
+    this.reviewService.postReview(this.ownReview).subscribe(res => console.log(this.ReviewMessage));
+  }
 }
---
+++ src/app/services/review.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {Observable} from 'rxjs';
+import { Review } from "../models/review.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReviewService {
+
+  apiEndpoint = environment.API_URL;
+  headers = new HttpHeaders();
+  options: {};
+
+  constructor(private http: HttpClient, public router: Router) {
+    this.headers.set('Content-Type', 'application/json');
+    this.options = {
+      headers: this.headers,
+      responseType: 'json',
+      withCredentials: true,
+      observe: 'response'
+    };
+  }
+
+  getReview(mediumId?: string): Observable<any>{
+    if (mediumId) {
+      mediumId = '?MediumId=' + mediumId;
+    }
+    const getReviews = `${this.apiEndpoint}/review/${mediumId || ''}`;
+    return this.http.get(getReviews, this.options);
+  }
+
+  postReview(review: Review): Observable<any>{
+    const postReview = `${this.apiEndpoint}/review`;
+    return this.http.post(postReview, review, this.options);
+  }
+
+  deleteReview(id: string): Observable<any>{
+    const deleteReview = `${this.apiEndpoint}/review/${id}`;
+    return this.http.delete(deleteReview, this.options);
+  }
+
+}
