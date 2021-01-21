b42bc09682c9f63cbf431172f0bd42f4ed4ed017
ZangerlA <azangerl@gmail.com>
2021-01-21 11:54:57 +0100

    change feedback profile, popup

--- src/app/components/home-profil/home-profil.component.ts
+++ src/app/components/home-profil/home-profil.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 import {AuthService} from '../../services/auth.service';
 import {ProfileEditService} from '../../services/profil.edit.service';
 import {Profile} from '../../models/profile.model';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatTabGroup} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-home-profil',
@@ -13,10 +15,11 @@ import {Profile} from '../../models/profile.model';
 export class HomeProfilComponent implements OnInit {
   editProfil: FormGroup;
   profile: Profile = new Profile();
+  @ViewChild('tabGroup') group: MatTabGroup;
 
 
 
-  constructor(private authService: AuthService, public fb: FormBuilder, public router: Router, public profileEditService: ProfileEditService) {
+  constructor(private authService: AuthService, public fb: FormBuilder, public router: Router, public profileEditService: ProfileEditService, public snackBar: MatSnackBar) {
     this.editProfil = this.fb.group({
       username: [''],
       user_email: [''],
@@ -34,11 +37,11 @@ export class HomeProfilComponent implements OnInit {
   getProfil(): void{
     this.profileEditService.getProfil().subscribe(res => {
       this.profile.id = res.body.id;
-      this.profile.name =res.body.username;
+      this.profile.name = res.body.username;
       this.profile.imgUrl = res.body.profileImgUrl;
       this.profile.description = res.body.userDescription;
-      this.profile.email =res.body.contact_email;
-    })
+      this.profile.email = res.body.contact_email;
+    });
   }
 
   edit(): void {
@@ -47,7 +50,14 @@ export class HomeProfilComponent implements OnInit {
     this.profileEditService.putProfil('contact_email', this.editProfil.value.user_email).subscribe(res => console.log(res));
     this.profileEditService.putProfil('pwHash', this.editProfil.value.password).subscribe(res => console.log(res));
     this.profileEditService.putProfil('profileImgUrl', this.editProfil.value.img_Url).subscribe(res => console.log(res));
-    this.profileEditService.putProfil('userDescription', this.editProfil.value.description).subscribe(res => console.log(res));
+    this.profileEditService.putProfil('userDescription', this.editProfil.value.description).subscribe(res => {
+      console.log(res);
+      this.getProfil();
+    });
+    this.snackBar.open('Success!', 'dismiss', {
+      duration: 2000, panelClass: ['mat-toolbar', 'mat-primary', 'custom-dialog-container']
+    });
+    this.group.selectedIndex = 0;
   }
   passwordValidator(form: FormGroup): {} {
     const condition = form.get('password').value !== form.get('password_control').value;
